aa42baf2f571bd4332c58a6323b8c24da03f147d
lshadler <lshadler13@gmail.com>
2020-05-02 13:45:41 -0700

    blog post template

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { graphql, Link } from 'gatsby';
 
+import styled from '@emotion/styled';
 import Page from '../components/Page';
 import Container from '../components/Container';
 import IndexLayout from '../layouts';
+import { colors } from '../styles/variables';
+
 
 interface PostTemplateProps {
   data: {
@@ -27,15 +30,30 @@ interface PostTemplateProps {
   }
 }
 
+const StyledLink = styled(Link)`
+  padding: 0.5em;
+  max-width: 300px;
+  text-align: center;
+  border: 1px solid ${colors.brand};
+`;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+`;
+
 const PostTemplate: React.FC<PostTemplateProps> = ({ data }) => (
   <IndexLayout>
     <Page>
-      <Container>
-        <Link to="/blog">Return to blog</Link>
-        <h1>{data.markdownRemark.frontmatter.title}</h1>
-        {/* eslint-disable-next-line react/no-danger */}
-        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-      </Container>
+      <StyledContainer>
+        <StyledLink to="/blog">Return to main blog page</StyledLink>
+        <div>
+          <h1>{data.markdownRemark.frontmatter.title}</h1>
+          {/* eslint-disable-next-line react/no-danger */}
+          <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        </div>
+        <StyledLink to="/blog" style={{ alignSelf: 'flex-end' }}>Return to main blog page</StyledLink>
+      </StyledContainer>
     </Page>
   </IndexLayout>
 );
