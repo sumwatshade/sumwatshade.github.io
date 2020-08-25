ca6b4ed6240798c00d9493b5a607ba46bbe3d935
lshadler <lshadler13@gmail.com>
2020-08-25 11:04:40 -0700

    fix: cleanup resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,15 +1,33 @@
 
 import * as React from 'react';
 
-import Img, { FluidObject } from 'gatsby-image';
-import { graphql } from 'gatsby';
 import styled from '@emotion/styled';
 import Page from '../components/Page';
 import IndexLayout from '../layouts';
-import { FluidImageQuery } from '../types.ts';
-import SkillBar from '../components/SkillBar';
-
+import { colors } from '../styles/variables';
 import { getEmSize } from '../styles/mixins';
+
+const ResumeTitle = styled.h1`
+  align-self: center;
+  margin: 30px;
+`;
+
+const CardTitle = styled.h2`
+  color: goldenrod;
+  align-self: center;
+`;
+
+const CardItemHeader = styled.h3`
+  color: goldenrod;
+`;
+
+const CardItemSubheaderHeader = styled.h4`
+  color: ${colors.ui.medium};
+  margin: 0 0 20px 0;
+`;
+
+const CardItemList = styled.ul``;
+const CardItemListItem = styled.li``;
 
 const ResumePage = styled(Page)`
   padding: 0;
@@ -17,249 +35,248 @@ const ResumePage = styled(Page)`
 
 const ResumeContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
   justify-content: space-evenly;
-
 `;
 
-interface LeftContentProps {
-  file: {
-    childImageSharp: {
-      fluid: FluidObject
-    }
-  }
-}
 
-const LeftContent = (data: LeftContentProps) => (
-  <div>
-    <Img fluid={data.file.childImageSharp.fluid} alt="Avatar" />
+/**
+ * Skills:
+ *
+ *  - math
+ *  - Data Analysis and Visualization
+ *  - Machine Learning
+ *  - Economic Game Theory
+ *  - Computational Astrophysics
+ *
+ * Languages
+ *  - Python
+ *  - Javascript
+ *  - C99
+ *  - Java
+ *  - C++
+ *  - Assembly (MIPS)
+ *  - MySQL
+ */
+
+const PersonalInfo = () => (
+  <>
+    <CardTitle>Contact Info</CardTitle>
     {/* TODO: Implement PDF resume here */}
-    <div>
-      <p>Developer and Scientist</p>
-      <p>New York, USA</p>
-      <p>lshadler13@gmail.com</p>
-      <p>(585)766-7886</p>
-      <hr />
-
-      <h2>Skills</h2>
-      <SkillBar skillName="Mathematics" pct="90" />
-      <SkillBar skillName="Data Analytics and Visualization" pct="85" />
-      <SkillBar skillName="Machine Learning" pct="60" />
-      <SkillBar skillName="Economic Game Theory" pct="40" />
-      <SkillBar skillName="Computational Astrophysics" pct="90" />
-
-      <h2>Languages</h2>
-      <SkillBar skillName="Python" pct="90" />
-      <SkillBar skillName="Javascript" pct="95" />
-      <SkillBar skillName="C99" pct="60" />
-      <SkillBar skillName="Java" pct="80" />
-      <SkillBar skillName="C++" pct="40" />
-      <SkillBar skillName="Assembly (MIPS)" pct="45" />
-      <SkillBar skillName="MySQL" pct="85" />
-    </div>
-  </div>
+    <CardItemList>
+      <CardItemListItem>San Diego, CA</CardItemListItem>
+      <CardItemListItem>lshadler13@gmail.com</CardItemListItem>
+      <CardItemListItem>(585)766-7886</CardItemListItem>
+    </CardItemList>
+  </>
 );
 
 const WorkExperience = () => (
   <>
-    <h2>Work Experience</h2>
+    <CardTitle>Work Experience</CardTitle>
     <div>
-      <h5><b>Full Stack Software Developer / Solu Technology Partners</b></h5>
-      <h6>July 2017 - January 2018</h6>
-      <p>
-        <ul>
-          <li>
-            Work as a member of a scrum team to provide state-of-the-art
-            Angular Material applications
-          </li>
-          <li>
-            Integrate a MySQL database architecture with front-end components
-            using the Spring Framework
-          </li>
-          <li>
-            Participate in code reviews to maintain a fast-paced
-            consumer-focused agenda
-          </li>
-        </ul>
-      </p>
+      <CardItemHeader>Full Stack Software Developer / Solu Technology Partners</CardItemHeader>
+      <CardItemSubheaderHeader>July 2017 - January 2018</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Work as a member of a scrum team to provide state-of-the-art
+          Angular Material applications
+        </CardItemListItem>
+        <CardItemListItem>
+          Integrate a MySQL database architecture with front-end components
+          using the Spring Framework
+        </CardItemListItem>
+        <CardItemListItem>
+          Participate in code reviews to maintain a fast-paced
+          consumer-focused agenda
+        </CardItemListItem>
+      </CardItemList>
     </div>
 
     <div>
-      <h5><b>Research and Development Engineer (Co-Op)/ WiTricity Corporation</b></h5>
-      <h6>August 2015 - January 2015</h6>
-      <p>
-        <ul>
-          <li>Developed auxiliary systems to aid in the safety of wireless power transfer</li>
-          <li>Prototyped embedded data acquisition systems from PCB design to fabrication</li>
-          <li>Firmware produced in C, visualized in Python with Qt Graphics toolkit</li>
-        </ul>
-      </p>
+      <CardItemHeader>
+        Research and Development Engineer (Co-Op)/ WiTricity Corporation
+      </CardItemHeader>
+      <CardItemSubheaderHeader>
+        August 2015 - January 2015
+      </CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Developed auxiliary systems to aid in the safety of wireless power transfer
+        </CardItemListItem>
+        <CardItemListItem>
+          Prototyped embedded data acquisition systems from PCB design to fabrication
+        </CardItemListItem>
+        <CardItemListItem>
+          Firmware produced in C, visualized in Python with Qt Graphics toolkit
+        </CardItemListItem>
+      </CardItemList>
     </div>
 
     <div>
-      <h5><b>Software Engineer / Intuit</b></h5>
-      <h6>February 2018 - Present</h6>
-      <p>
-        <ul>
-          <li>
-            Develop industry-leading experiences for authentication/authorization within
-            the Financial sphere
-          </li>
-          <li>
-            Collaboration across several teams in a large company to develop robust
-            solutions with great velocity
-          </li>
-          <li>
-            Uncompromising work ensuring that the product meets company standards and
-            creates customer delight
-          </li>
-          <li>
-            Relevant Technologies: Javascript, ReactJS, Java, Spring/Spring Boot,
-            Webpack, Python, Atlassian (JIRA)
-          </li>
-        </ul>
-      </p>
+      <CardItemHeader>Software Engineer / Intuit</CardItemHeader>
+      <CardItemSubheaderHeader>February 2018 - Present</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Develop industry-leading experiences for authentication/authorization within
+          the Financial sphere
+        </CardItemListItem>
+        <CardItemListItem>
+          Collaboration across several teams in a large company to develop robust
+          solutions with great velocity
+        </CardItemListItem>
+        <CardItemListItem>
+          Uncompromising work ensuring that the product meets company standards and
+          creates customer delight
+        </CardItemListItem>
+        <CardItemListItem>
+          Relevant Technologies: Javascript, ReactJS, Java, Spring/Spring Boot,
+          Webpack, Python, Atlassian (JIRA)
+        </CardItemListItem>
+      </CardItemList>
     </div>
   </>
 );
 
 const AcademicExperience = () => (
   <>
-    <h2>Academic Projects</h2>
+    <CardTitle>Academic Experience</CardTitle>
     <div>
-      <h5><b>Improving the Stochastic Template Bank Algorithm for aLIGO / Caltech</b></h5>
-      <h6>Summer 2016</h6>
-      <p>
-        <ul>
-          <li>Developed an improved method of analyzing Gravitational Wave data</li>
-          <li>
-            Use Markov Chain Monte Carlo methods with the benefits of parallel
-            computing on powerful multi-core machines
-          </li>
-          <li>Written in C utilizing the OpenMP framework</li>
-        </ul>
-      </p>
+      <CardItemHeader>
+        Improving the Stochastic Template Bank Algorithm for aLIGO / Caltech
+      </CardItemHeader>
+      <CardItemSubheaderHeader>Summer 2016</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Developed an improved method of analyzing Gravitational Wave data
+        </CardItemListItem>
+        <CardItemListItem>
+          Use Markov Chain Monte Carlo methods with the benefits of parallel
+          computing on powerful multi-core machines
+        </CardItemListItem>
+        <CardItemListItem>Written in C utilizing the OpenMP framework</CardItemListItem>
+      </CardItemList>
     </div>
     <div>
-      <h5><b>Optimization of MINERvA Proton Selection Algorithm / FermiLab, IL</b></h5>
-      <h6>Fall 2014 - Summer 2015</h6>
-      <p>
-        <ul>
-          <li>
-            Wrote analysis and algorithmic code to calculate the energy of
-            incoming neutrinos in a Neutral-Current Elastic reaction
-          </li>
-          <li>Work with the MINERvA detector, a large collaboration at Fermilab</li>
-          <li>Extended to an REU at the University of Rochester</li>
-        </ul>
-      </p>
+      <CardItemHeader>
+        Optimization of MINERvA Proton Selection Algorithm / FermiLab, IL
+      </CardItemHeader>
+      <CardItemSubheaderHeader>Fall 2014 - Summer 2015</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Wrote analysis and algorithmic code to calculate the energy of
+          incoming neutrinos in a Neutral-Current Elastic reaction
+        </CardItemListItem>
+        <CardItemListItem>
+          Work with the MINERvA detector, a large collaboration at Fermilab
+        </CardItemListItem>
+        <CardItemListItem>Extended to an REU at the University of Rochester</CardItemListItem>
+      </CardItemList>
     </div>
     <div>
-      <h5><b>Feeding and Feedback of Active Galactic Nuclei</b></h5>
-      <h6>2013 - 2015</h6>
-      <p>
-        <ul>
-          <li>
-            Ran three-dimensional morpho-kinematical simulations aimed at confirming
-            and analyzing the anticipated gas kinematics of active galactic nuclei
-          </li>
-          <li>Code developed in Java with collaboration with several groups</li>
-          <li>
-            Led a week-long workshop at Universidade Federal do Rio Grande do
-            Sol in Porto Allegre, Brazil
-          </li>
-        </ul>
-      </p>
+      <CardItemHeader>Feeding and Feedback of Active Galactic Nuclei</CardItemHeader>
+      <CardItemSubheaderHeader>2013 - 2015</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          Ran three-dimensional morpho-kinematical simulations aimed at confirming
+          and analyzing the anticipated gas kinematics of active galactic nuclei
+        </CardItemListItem>
+        <CardItemListItem>
+          ode developed in Java with collaboration with several groups
+        </CardItemListItem>
+        <CardItemListItem>
+          Led a week-long workshop at Universidade Federal do Rio Grande do
+          Sol in Porto Allegre, Brazil
+        </CardItemListItem>
+      </CardItemList>
     </div>
     <div>
-      <h5><b>Research Assistant @ Laboratory for Laser Energetics</b></h5>
-      <h6>2013 - 2015</h6>
-      <p>
-        <ul>
-          <li>
-            Developed a program using Matlab in order to analyze the flow rate of
-            reaction from the main laser target bay into the waste systems
-          </li>
-          <li>
-            Analysis would calculate the current capacity of the molecular sieve
-            beds and prompt workers when the system would require maintenance
-          </li>
-          <li>
-            Benefits to efficient task scheduling and limiting the risk of
-            radiation leakage
-          </li>
-        </ul>
-      </p>
+      <CardItemHeader>Research Assistant @ Laboratory for Laser Energetics</CardItemHeader>
+      <CardItemSubheaderHeader>2013 - 2015</CardItemSubheaderHeader>
+
+      <CardItemList>
+        <CardItemListItem>
+          Developed a program using Matlab in order to analyze the flow rate of
+          reaction from the main laser target bay into the waste systems
+        </CardItemListItem>
+        <CardItemListItem>
+          Analysis would calculate the current capacity of the molecular sieve
+          beds and prompt workers when the system would require maintenance
+        </CardItemListItem>
+        <CardItemListItem>
+          Benefits to efficient task scheduling and limiting the risk of
+          radiation leakage
+        </CardItemListItem>
+      </CardItemList>
+
     </div>
   </>
 );
 
 const Education = () => (
   <>
-    <h2>Education</h2>
+    <CardTitle>Education</CardTitle>
     <div>
-      <h5><b>Rochester Institute of Technology</b></h5>
-      <h6>2013-2017</h6>
-      <p>Physics, B.S.</p>
-      <p>
-        Relevant Coursework:
-        <ul>
-          <li>
-            <b>Computer Science:</b>
-            {' '}
-            Principles of C, Computer Science Theory, Computer Architecture,
-            Compilers, Data Structures and Algorithms, Databases
-          </li>
-          <li>
-            <b>Mathematics:</b>
-            {' '}
-            Multi-variable Calculus, Probability and Statistics, Differential
-            Equations, Advanced Linear Algebra, Numerical Linear Algebra
-          </li>
-          <li>
-            <b>Physics:</b>
-            {' '}
-            Electricity and Magnetism, Classical Mechanics, Statistical Mechanics,
-            Quantum Mechanics, Solid State Physics, Physical Optics
-          </li>
-          <li>
-            <b>Engineering:</b>
-            {' '}
-            Electronic Measurements, Circuits (AC/DC), Electronics I
-          </li>
-        </ul>
-      </p>
+      <CardItemHeader>Physics, B.S. - Rochester Institute of Technology</CardItemHeader>
+      <CardItemSubheaderHeader>2013-2017</CardItemSubheaderHeader>
+      <CardItemList>
+        <CardItemListItem>
+          <b>Computer Science:</b>
+          {' '}
+          Principles of C, Computer Science Theory, Computer Architecture,
+          Compilers, Data Structures and Algorithms, Databases
+        </CardItemListItem>
+        <CardItemListItem>
+          <b>Mathematics:</b>
+          {' '}
+          Multi-variable Calculus, Probability and Statistics, Differential
+          Equations, Advanced Linear Algebra, Numerical Linear Algebra
+        </CardItemListItem>
+        <CardItemListItem>
+          <b>Physics:</b>
+          {' '}
+          Electricity and Magnetism, Classical Mechanics, Statistical Mechanics,
+          Quantum Mechanics, Solid State Physics, Physical Optics
+        </CardItemListItem>
+        <CardItemListItem>
+          <b>Engineering:</b>
+          {' '}
+          Electronic Measurements, Circuits (AC/DC), Electronics I
+        </CardItemListItem>
+      </CardItemList>
     </div>
   </>
 );
 
 
-const LeftPanel = styled.div`
+const Card = styled.section`
   background-color: var(--bg);
   border: ${getEmSize(1)}em solid var(--textTitle);
   border-radius: 5px;
   padding: 2em;
   margin: 1em;
-  flex: 1;
+  display: flex;
+  flex-direction: column;
 `;
 
-const RightPanel = styled(LeftPanel)`
-  flex: 2;
-`;
-
-const Resume = ({ data }: FluidImageQuery) => (
+const Resume = () => (
   <IndexLayout>
     <ResumePage>
       <ResumeContainer>
-        <LeftPanel>
-          <LeftContent file={data.file} />
-        </LeftPanel>
-        <RightPanel>
+        <ResumeTitle>Lucas Shadler - Resume</ResumeTitle>
+        <Card>
           <WorkExperience />
+        </Card>
+        <Card>
           <AcademicExperience />
+        </Card>
+        <Card>
           <Education />
-        </RightPanel>
+        </Card>
+        <Card>
+          <PersonalInfo />
+        </Card>
       </ResumeContainer>
     </ResumePage>
   </IndexLayout>
@@ -267,16 +284,16 @@ const Resume = ({ data }: FluidImageQuery) => (
 
 export default Resume;
 
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "Luke_portrait_01.jpg" }) {
-      childImageSharp {
-          # Specify the image processing specifications right in the query.
-          # Makes it trivial to update as your page's design changes.
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query {
+//     file(relativePath: { eq: "Luke_portrait_01.jpg" }) {
+//       childImageSharp {
+//           # Specify the image processing specifications right in the query.
+//           # Makes it trivial to update as your page's design changes.
+//           fluid {
+//             ...GatsbyImageSharpFluid
+//           }
+//       }
+//     }
+//   }
+// `;
