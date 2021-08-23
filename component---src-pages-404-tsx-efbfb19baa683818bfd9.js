c94d6c0e73282e71cf362b7c760ab2c723d0751f
lshadler <lshadler13@gmail.com>
2021-08-23 23:35:41 +0000

    deploy site [skip ci]

---
+++ component---src-pages-404-tsx-efbfb19baa683818bfd9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklshadler_github_io=self.webpackChunklshadler_github_io||[]).push([[218],{938:function(t,l,u){u.r(l);u(7294);var n=u(5444),e=u(7614),h=u(832),o=u(3670),i=u(427);l.default=function(){return(0,i.tZ)(o.Z,null,(0,i.tZ)(e.Z,null,(0,i.tZ)(h.Z,null,(0,i.tZ)("h1",null,"404: Page not found."),(0,i.tZ)("p",null,"You've hit the void."," ",(0,i.tZ)(n.rU,{to:"/"},"Go back.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-efbfb19baa683818bfd9.js.map
